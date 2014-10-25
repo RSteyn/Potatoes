bd60ca1a921ba2fdfeffdf3b37552c55a957efc6
rileysteyn <riley@steyn.co.nz>
2014-10-25 19:01:23 +1300

    Updated Vector class to be more useful in polar form.  Can now rotate and get direction easily.

--- potatoes/vector.py
+++ potatoes/vector.py
@@ -12,6 +12,13 @@ class Vector:
         pol_vect[0] = 1
         pol_vect = Vector(pol_vect[0], pol_vect[1], True)
         return pol_vect
+    @staticmethod
+    def rotate(vector, theta):
+        # Rotate vector by theta, anticlockwise if theta is positive,
+        # clockwise if negative.
+        new_x = vector.x*math.cos(theta) - vector.y*math.sin(theta)
+        new_y = vector.x*math.sin(theta) + vector.y*math.cos(theta)
+        return Vector(new_x, new_y)
 
     def __init__(self, x, y, polar=False):
         if not polar:
@@ -39,6 +46,10 @@ class Vector:
         return self.__mul__(other)
     def __str__(self):
         return '(' + str(self.x) + ', ' + str(self.y) + ')'
+
+    @property
+    def direction(self):
+        return math.atan2(self.y, self.x)
 
     def get_len_squared(self):
         return self.x**2 + self.y**2
