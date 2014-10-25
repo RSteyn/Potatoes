1e0382c9cf5f204ea79f548122ca3347acb7eb46
rileysteyn <riley@steyn.co.nz>
2014-10-25 19:26:32 +1300

    Create basic Asteroid class.

---
+++ potatoes/asteroid.py
@@ -0,0 +1,19 @@
+__author__ = 'rileysteyn'
+from .entity import *
+import random
+import math
+class Asteroid(Entity, Movable, Renderable):
+    MAX_VEL = 5 # TODO: Balance this
+    MIN_VEL = 2 # TODO: Balance this
+    VEL_RANGE = MAX_VEL - MIN_VEL
+    def __init__(self, canvas):
+        Entity.__init__(self)
+        Movable.__init__(self)
+        Renderable.__init__(self, self._pos.x, self._pos.y,
+                            'resources/dean.gif', canvas)
+
+        dir = 2*math.pi * random.random()
+        vel = Asteroid.VEL_RANGE * random.random() + Asteroid.MIN_VEL
+        self._moving = Vector(vel, dir, polar=True)
+    def update(self, delta, gx):
+        self.move()
