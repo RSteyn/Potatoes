f2b6af4f1ea71a5d6656198e7dd87c29d000407e
Nelson Crosby <nc@sourcecomb.com>
2014-10-25 16:21:45 +1300

    Attributes don't need init methods

--- potatoes/entity.py
+++ potatoes/entity.py
@@ -10,25 +10,16 @@ class Entity:
 
 
 class Renderable:
-    def __init__(self):
-        pass
-
     def render(self, gx):
         pass
 
 
 class Shootable:
-    def __init__(self):
-        pass
-
     def shoot(self, x, y):
         pass
 
 
 class Movable:
-    def __init__(self):
-        pass
-
     def move(self, x, y):
         self._x += x
         self._y += y
