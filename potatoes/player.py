99eeccb292fcbf9ca3a7ca36173242e9c8b9bbd5
Nelson Crosby <nc@sourcecomb.com>
2014-10-25 17:50:35 +1300

    Constants

--- potatoes/player.py
+++ potatoes/player.py
@@ -2,6 +2,11 @@ from .entity import *
 
 
 class Player(Entity, Movable, Renderable, Shootable):
+    UP = -1
+    DOWN = 1
+    LEFT = -1
+    RIGHT = 1
+
     def __init__(self, bind_to):
         Entity.__init__(self)
         Movable.__init__(self)
@@ -9,24 +14,32 @@ class Player(Entity, Movable, Renderable, Shootable):
         Shootable.__init__(self)
 
         self._vel = 5           # TODO: Balance this
-        bind_to.bind('<KeyPress-Up>', lambda _: self.start_moving_y(-1))
-        bind_to.bind('<KeyPress-Down>', lambda _: self.start_moving_y(1))
-        bind_to.bind('<KeyPress-Left>', lambda _: self.start_moving_x(-1))
-        bind_to.bind('<KeyPress-Right>', lambda _: self.start_moving_x(1))
+        bind_to.bind('<KeyPress-Up>',
+                     lambda _: self.start_moving_y(self.UP))
+        bind_to.bind('<KeyPress-Down>',
+                     lambda _: self.start_moving_y(self.DOWN))
+        bind_to.bind('<KeyPress-Left>',
+                     lambda _: self.start_moving_x(self.LEFT))
+        bind_to.bind('<KeyPress-Right>',
+                     lambda _: self.start_moving_x(self.RIGHT))
 
-        bind_to.bind('<KeyRelease-Up>', lambda _: self.stop_moving_y())
-        bind_to.bind('<KeyRelease-Down>', lambda _: self.stop_moving_y())
-        bind_to.bind('<KeyRelease-Left>', lambda _: self.stop_moving_x())
-        bind_to.bind('<KeyRelease-Right>', lambda _: self.stop_moving_x())
+        bind_to.bind('<KeyRelease-Up>',
+                     lambda _: self.stop_moving_y())
+        bind_to.bind('<KeyRelease-Down>',
+                     lambda _: self.stop_moving_y())
+        bind_to.bind('<KeyRelease-Left>',
+                     lambda _: self.stop_moving_x())
+        bind_to.bind('<KeyRelease-Right>',
+                     lambda _: self.stop_moving_x())
 
-    def start_moving_x(self, direction=1):
+    def start_moving_x(self, direction=DOWN):
         self.moving.x *= self._vel * direction
 
     def stop_moving_x(self):
         self.moving.x = 0
 
-    def start_moving_y(self, direction=1):
-        self.moving.y *= -self._vel * direction
+    def start_moving_y(self, direction=RIGHT):
+        self.moving.y *= self._vel * direction
 
     def stop_moving_y(self):
         self.moving.y = 0
